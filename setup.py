96abcc52a30d9c572ae2f3f6f55380de5a43426e
allfsolutions <103066471+allfsolutions@users.noreply.github.com>
2022-04-06 12:14:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_pip_package',
+    name='my_py_package',
     version=0.1,
 
     url='https://github.com/allfsolutions/my_py_package',
